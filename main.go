a2cd835bb43d4efc17238414fd32bef5af54bf54
roaris <one0803kyopro@gmail.com>
2022-01-20 20:39:51 +0900

    todoの作成

--- main.go
+++ main.go
@@ -17,26 +17,29 @@ func main() {
 
 	fmt.Println(models.DB)
 
+	u := &models.User{}
+	u.Name = "test"
+	u.Email = "test@example.com"
+	u.Password = "password"
+	fmt.Println(u)
+
+	u.CreateUser()
+
 	/*
-		u := &models.User{}
-		u.Name = "test"
-		u.Email = "test@example.com"
-		u.Password = "password"
+		u, _ := models.GetUser(1)
 		fmt.Println(u)
 
-		u.CreateUser()
+		u.Name = "test2"
+		u.Email = "test2@example.com"
+		u.UpdateUser()
+		u, _ = models.GetUser(1)
+		fmt.Println(u)
+
+		u.DeleteUser()
+		u, _ = models.GetUser(1)
+		fmt.Println(u)
 	*/
 
-	u, _ := models.GetUser(1)
-	fmt.Println(u)
-
-	u.Name = "test2"
-	u.Email = "test2@example.com"
-	u.UpdateUser()
-	u, _ = models.GetUser(1)
-	fmt.Println(u)
-
-	u.DeleteUser()
-	u, _ = models.GetUser(1)
-	fmt.Println(u)
+	user, _ := models.GetUser(2)
+	user.CreateTodo("First Todo")
 }
